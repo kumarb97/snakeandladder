1322b2201fe3a731356fdd69a7647d163fdff0ea
kumarb97 <kumarbamankar@gmail.com>
2022-02-16 08:36:45 +0530

    snake and ladder second use case added

---
+++ Snake_Ladder_UC2.java
@@ -0,0 +1,12 @@
+package snakeandladder;
+
+public class Snake_Ladder_UC2 {
+
+	public static void main(String[] args) {
+		
+		int dice = (int) Math.floor(((Math.random() * 10) % 6) + 1 );
+		System.out.println("dice value is " +dice);
+
+	}
+
+}
