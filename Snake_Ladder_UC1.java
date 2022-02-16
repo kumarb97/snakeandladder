df22585b699b8c1eca4e3733161cf05247e4f9bd
kumarb97 <kumarbamankar@gmail.com>
2022-02-16 08:35:04 +0530

    snake and ladder first use case added

---
+++ Snake_Ladder_UC1.java
@@ -0,0 +1,11 @@
+package snakeandladder;
+
+public class Snake_Ladder_UC1 {
+
+	public static void main(String[] args) {
+		int points = 0;
+		System.out.println("Snake and ladder game with single player at start position " +points);
+
+	}
+
+}
